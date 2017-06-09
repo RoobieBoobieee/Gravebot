07458e60e297fb859f195155e35df47cab74fa4d
Rob <robvankeilegom@hotmail.com>
2017-06-09 14:47:56 +0200

    added days to uptime

--- src/commands/info/uptime.js
+++ src/commands/info/uptime.js
@@ -6,7 +6,8 @@ import { getShardsCmdResults } from '../../redis';
 
 
 function uptime(client, evt, suffix, lang, json) {
-  const uptimeh = Math.floor(process.uptime() / (60 * 60));
+  const uptimed = Math.floor(process.uptime() / (60 * 60 * 24));
+  const uptimeh = Math.floor(process.uptime() % (60 * 60 * 24) / (60 * 60));
   const uptimem = Math.floor(process.uptime() % (60 * 60) / 60);
   const uptimes = Math.floor(process.uptime() % 60);
 
@@ -17,6 +18,7 @@ function uptime(client, evt, suffix, lang, json) {
       .map(({shard, results}) => {
         if (!results || R.isEmpty(results)) return `Shard ${shard} isn't live yet.`;
         return `Shard ${shard} has been alive for:
+${results.uptimed} Days
 ${results.uptimeh} Hours
 ${results.uptimem} Minutes
 ${results.uptimes} Seconds`;
